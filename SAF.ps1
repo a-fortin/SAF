705454787bf0659ab770aa31203b58a34f8c8ad0
Antoine Fortin <104103115+a-fortin@users.noreply.github.com>
2023-12-28 16:16:44 -0500

    Update 1.1
    + Colors
    + Require Admin
    + Fix Servers with no SA

--- SAF.ps1
+++ SAF.ps1
@@ -11,20 +11,28 @@ Check active Directory for servers and then check Win32_Service for any service 
 service accounts on a table in C:\temp\report.html
 
 .INFO
-Version:        1.0.0
+Version:        1.1.0
 Author:         Antoine Fortin
 Co-Author:      Olivier Magny
 Date:           28/12/2023
 #>
 
-#---------------------------------------------------------[Retrive Servers in AD]--------------------------------------------------------
+#
+if (-not (New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator)) {
+# Prompt the user to elevate the script
+$arguments = "& '" + $myInvocation.MyCommand.Definition + "'"
+Start-Process powershell -Verb runAs -ArgumentList $arguments
+exit
+}
+
+#---------------------------------------------------------[Retrive Servers in AD]------------------------------------------------
 Write-Host 'Fetchings Servers...'
 $Computers = Get-ADComputer -Filter {enabled -eq $true -and operatingsystem -like "*server*"} -properties *|select Name, DNSHostName, Enabled, Operatingsystem
 
 #---------------------------------------------------------[Define String]--------------------------------------------------------
 $OnlineComputerNames = @()
 
-#---------------------------------------------------------[Test connectivity for each Servers]-------------------------------------------------------
+#---------------------------------------------------------[Test connectivity for each Servers]-----------------------------------
 foreach ($Server in $Computers){
     $Hostname =$Server.DNSHostName
     $Pingtest = Test-Connection -ComputerName $Hostname -Quiet -Count 1 -ErrorAction SilentlyContinue
@@ -39,30 +47,34 @@ Write-Host 'Servers Fetched :'
 $OnlineComputerNames = $OnlineComputerNames|Sort
 Write-Host $OnlineComputerNames
 
-#---------------------------------------------------------[HTML content]-------------------------------------------------------
+#---------------------------------------------------------[HTML content]---------------------------------------------------------
 $HTML = @"
 <title>Service Account Script</title>
 <style>
-    body {background-color:#FFFFF}
-    table {Border-width:thin;border-style:solid;border-color:Black;border-collapse:collapse;}
-    th {border-width:1px;padding:2px;border-style:solid;border-color:black;background-color:ThreeDShadow;color:white}
-    td {border-width:1px;padding:2px;border-style:solid;border-color:black;background-color:Transparent}
+    html {margin: 0;background: linear-gradient(45deg, #49a09d, #5f2c82);font-family: sans-serif;font-weight: 100;}
+    table {width: 800px;border-collapse: collapse;overflow: hidden;box-shadow: 0 0 20px rgba(0,0,0,0.1);}
+    th,td {padding: 15px;background-color: rgba(255,255,255,0.2);color: #fff;}
+    th {background-color: #55608f;text-align: left;}
+    tbody {tr {&:hover {background-color: rgba(255,255,255,0.3);}}td {position: relative;&:hover {&:before {content: "";position: absolute;left: 0;right: 0;top: -9999px;bottom: -9999px;background-color: rgba(255,255,255,0.2);z-index: -1;}}}}
+    h1,h2 {color: white;}
 </style>
 "@
 
-#---------------------------------------------------------[Define the report file path]-------------------------------------------------------
+#---------------------------------------------------------[Define the report file path]------------------------------------------
 $Report = "C:\temp\report.html"
 Clear-Content -Path "$Report"
 
 
-#---------------------------------------------------------[Loop through each computer]-------------------------------------------------------
+#---------------------------------------------------------[Loop through each computer]-------------------------------------------
 Write-Host 'Generating Report...'
 ForEach ($SRV in $OnlineComputerNames) {
-    Get-WmiObject -ComputerName $SRV -Class Win32_Service -ErrorAction SilentlyContinue | Where-Object {
-        $_.StartName -notin @("LocalSystem", "NT AUTHORITY\NetworkService", "NT AUTHORITY\Network Service", "NT AUTHORITY\LocalService", "NT AUTHORITY\Local Service", $null)
-    } if($_)|Sort | Select-Object -Property StartName, Name, DisplayName | ConvertTo-Html -Property StartName, Name, DisplayName -Head $HTML -Body "<H2>$SRV</H2>" | Out-File -Append -FilePath $Report
+    $Services = Get-WmiObject -ComputerName $SRV -Class Win32_Service -ErrorAction SilentlyContinue | Where-Object {
+        $_.StartName -notin @("LocalSystem", "NT AUTHORITY\NetworkService", "NT AUTHORITY\Network Service", "NT AUTHORITY\LocalService", "NT AUTHORITY\Local Service", $null)}
+    if ($Services.Count -gt 0) {
+    $Services | Sort | Select-Object -Property StartName, Name, DisplayName | ConvertTo-Html -Property StartName, Name, DisplayName -Head $HTML -Body "<H2>$SRV</H2>" | Out-File -Append -FilePath $Report
+    }
 }
 
-#---------------------------------------------------------[Open the report]-------------------------------------------------------
+#---------------------------------------------------------[Open the report]------------------------------------------------------
 Invoke-Item $Report
 Write-Host @("Report Generated. location $Report")
